5b1dc56c98d016e5bde1e8c6a8a0aedeefdcfc57
GabrielVasco13 <97997902+GabrielVasco13@users.noreply.github.com>
2022-05-18 10:33:54 -0300

    adicionando o codigo na nuvem

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,42 @@
+import { Negociacao } from "../dist/js/models/negociacao.js";
+import { Negociacoes } from "../dist/js/models/negociacoes.js";
+
+export class NegociacoesView {
+
+    private elemento: HTMLElement;
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+    template(model: Negociacoes): string {
+        return`
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${model.lista().map(Negociacao => {
+                    return `
+                        <tr>
+                            <td>?</td>
+                            <td>${Negociacao.quantidade}</td>
+                            <td>${Negociacao.valor}</td>
+                        </tr>
+                    `;
+                
+                }).join('')}
+            </tbody>
+        </table>
+    `;
+    }
+
+    update(model: Negociacoes): void{
+        const template = this.template(model);
+        console.log(template);
+        this.elemento.innerHTML = template;
+    }
+}
